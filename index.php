e8ccd07ac4101bfcde5077dbf44f45cabf20c4f4
fikri47 <fadillahfikri48@gmail.com>
2020-07-28 13:08:46 +0700

    menambah list baru

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, repellat, beatae doloribus sequi vero error reprehenderit hic, ipsam quaerat tenetur animi! Ut illo atque quia quasi quaerat quam beatae. Dolore!</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+    </ul>
 </body>
 </html>
